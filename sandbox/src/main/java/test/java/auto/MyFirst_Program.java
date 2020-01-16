1f90beac81684797b11ecd36749851a373782d56
Python_test <evgeniy.makuta.job@gmail.com>
2020-01-16 22:15:44 +0300

    Finished 1 lesson of JAVA. Functions/classes/objects

--- sandbox/src/main/java/test/java/auto/MyFirst_Program.java
+++ sandbox/src/main/java/test/java/auto/MyFirst_Program.java
@@ -5,8 +5,31 @@ public class MyFirst_Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello, world!");
+		Square s = new Square(6);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+		Square ss = new Square(8);
+		System.out.println("Площадь квадрата2 со стороной " + ss.l + " = " + ss.area());
+
+		Rectangle r = new Rectangle(2, 3);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+		Point p = new Point(4, 8);
+		System.out.println("Дистанция между x и y = " + p.distance());
+
+
+		System.out.println("Квадратный корень из = " + p.x + " = " + Math.sqrt(p.x));
+		System.out.println("Квадратный корень из = " + p.y + " = " + Math.sqrt(p.y));
+
 
 }
+
+
+
+
+
+
+
+
 
 }
---
+++ sandbox/src/main/java/test/java/auto/Point.java
@@ -0,0 +1,16 @@
+package test.java.auto;
+
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance() {
+    return this.y - this.x;
+  }
+
+}
